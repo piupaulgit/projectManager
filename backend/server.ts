86c9f56b63af19eabd99d5c824300ec8a483d58a
piupaulgitm <contactpiu619@gmail.com>
2022-02-23 23:48:26 +0530

    database connection

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,9 @@
+import mongoose from "mongoose"
+
 const express = require('express')
 const app = express()
 const users = require('./routes/user')
+mongoose.connect(process.env.DATABASE)
 app.get('/', users)
 // port
 const port = process.env.PORT || 5000;
